aff9476f31a1dc65b601839a0ec3738661095813
qmarque <quentinm2000@hotmail.fr>
2021-11-05 11:48:19 +0100

    Affichage de la map

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleMap from './components/GoogleMap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>Map</h1>
+        <GoogleMap />
       </header>
     </div>
   );
