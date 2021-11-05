57af34f2a6545c54199da74da3459fa73e31c52c
qmarque <quentinm2000@hotmail.fr>
2021-11-05 16:54:00 +0100

    Mise en place de la recherche par ville

--- front/src/components/GoogleMap.js
+++ front/src/components/GoogleMap.js
@@ -1,53 +1,105 @@
 import React, { Component } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+
 export class MapContainer extends Component {
-    state = {
-        showingInfoWindow: false,
-        activeMarker: {},
-        selectedPlace: {},
-        mapCenter: {
-            lat: 44.8333,
-            lng: -0.56667
+    constructor(props) {
+        super(props);
+        this.state = {
+            address: '',
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+            mapCenter: {
+                lat: 44.8333,
+                lng: -0.56667
+            }
+
+
         }
     };
 
-    onMarkerClick = (props, marker, e) =>
-        this.setState({
-            selectedPlace: props,
-            activeMarker: marker,
-            showingInfoWindow: true
-        });
+    handleChange = address => {
+        this.setState({ address });
+    };
 
-    onMapClicked = (props) => {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-                showingInfoWindow: false,
-                activeMarker: null
+    handleSelect = address => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+                console.log('Success', latLng);
+                this.setState({ address });
+                this.setState({ mapCenter: latLng });
             })
-        }
+            .catch(error => console.error('Error', error));
     };
 
     render() {
         return (
-            <Map google={this.props.google}
-                initialCenter={{
-                    lat: this.state.mapCenter.lat,
-                    lng: this.state.mapCenter.lng,
-                }}
-                center={{
-                    lat: this.state.mapCenter.lat,
-                    lng: this.state.mapCenter.lng,
-                }}>
-                <Marker
-                    position={{
+            <div id="googleMap">
+                <PlacesAutocomplete
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelect}
+                    onError={this._handleError}
+                    clearItemsOnError={true}
+                >
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <div>
+                            <input
+                                {...getInputProps({
+                                    placeholder: 'Rechercher une ville ...',
+                                    className: 'location-search-input',
+                                })}
+                            />
+                            <div className="autocomplete-dropdown-container">
+                                {loading && <div>Loading...</div>}
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active
+                                        ? 'suggestion-item--active'
+                                        : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                    return (
+                                        <div
+                                            {...getSuggestionItemProps(suggestion, {
+                                                className,
+                                                style,
+                                            })}
+                                        >
+                                            <span>{suggestion.description}</span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    )}
+                </PlacesAutocomplete>
+                <Map google={this.props.google}
+                    initialCenter={{
                         lat: this.state.mapCenter.lat,
                         lng: this.state.mapCenter.lng,
-                    }} />
-            </Map>
+                    }}
+                    center={{
+                        lat: this.state.mapCenter.lat,
+                        lng: this.state.mapCenter.lng,
+                    }}>
+                    {/* <Marker
+                        position={{
+                            lat: this.state.mapCenter.lat,
+                            lng: this.state.mapCenter.lng,
+                        }} /> */}
+                </Map>
+            </div>
         )
     }
 }
 
 export default GoogleApiWrapper({
-    apiKey: ('AIzaSyDlJMb54LN9OvrECOrg8rMNPkVJIjkbe0k')
+    apiKey: ('AIzaSyCyLts9prer-LXZW2WNWSAJR-TWu3E_Q9w&librairies=places')
 })(MapContainer)
